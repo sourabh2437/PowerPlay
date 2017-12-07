ec00fc2deea5f9cbae19eaeb60639c5b258f45eb
Gaurav Bansal <grvbansal1995@gmail.com>
2017-12-07 20:31:42 +0530

    Filtering based on Location and slot

--- src/Components/BadmintonCourts.js
+++ src/Components/BadmintonCourts.js
@@ -1,34 +1,54 @@
 import React, { Component } from 'react';
-import data from '../testData.json';
+import bookingData from '../data/Booking.json';
 import CourtPreview from './CourtPreview';
 import AppHeader from './AppHeader';
 import Filterbar from './FilterBar';
+import SlotFilter from './SlotFilter';
 
 class BadmintonCourts extends Component {
     
     state = {
         pageHeader: 'Badminton',
         pageDesc:'Come on Shuttlers!!!',
-        location: 0
+        location: 'l1',
+        date: 'd1',
+        slot: 's1'        
     };
-    handleSelect = (value) => {
+
+
+    handleSelect1 = (value) => {
     this.setState({ location  : value });
-  }
+    }
+    
+    handleSelect2 = (value) => {
+    this.setState({ slot  : value });
+    }
+    
+    
     render() {
+        
+    var aLocation=this.state.location;
+    var aDate=this.state.date;
+    var aSlot=this.state.slot;
+        
+    var aData= bookingData.badminton;
+        
     return (
         
             <div>
             <div className="row">
                 <AppHeader header ={this.state.pageHeader} 
                      desc ={this.state.pageDesc} /></div>
-             <div className="row" id="appRow"> 
-        <div className="col-sm-4"><label>Select Location</label><Filterbar onSelect={this.handleSelect}/></div>
-        <div className="col-sm-4"><label>Select Date</label><Filterbar/></div>
-        <div className="col-sm-4"><label>Select Slot</label><Filterbar/></div>
-        </div>
+        
+                <div className="row" id="appRow"> 
+                    <div className="col-sm-4"><label>Select Location</label><Filterbar onSelect={this.handleSelect1}/></div>
+                    <div className="col-sm-4"><label>Select Date</label><SlotFilter onSelect={this.handleSelect2} /></div>
+                    <div className="col-sm-4"><label>Select Slot</label><Filterbar/></div>
+                </div>
+        
                 <div className="row" >
-                    {data.badminton.location[this.state.location].map(itr =>
-                      <CourtPreview image={"images/badminton_tile.jpg"} key={itr.id} {...itr} />
+                    {aData[aLocation][aDate][aSlot].map(itr =>
+                      <CourtPreview image={"images/badminton_tile.jpg"} courtName="A" courtLocation="AB" />
                     )}
                 </div>
         
--- src/Components/SlotFilter.js
+++ src/Components/SlotFilter.js
@@ -1,18 +1,14 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import Dropdown from 'react-dropdown';
 import Select from 'react-select';
-import 'react-select/dist/react-select.css';
 import { Link } from 'react-router-dom';
 import ConfirmBooking from './ConfirmBooking'; 
 import {OverlayTrigger, Tooltip, Popover,Button,Modal} from 'react-bootstrap';
 
-
-class Filterbar extends Component{
-    
-  state = {
+class SlotFilter extends Component{
+ state = {
     selectedOption: '',
-    value: 'l1'
+    value: 's0'
   }
 
   handleChange = (selectedOption) => {
@@ -22,21 +18,26 @@ class Filterbar extends Component{
       
     this.props.onSelect(selectedOption.value);
   }
-  
+
   render() {
     return (
       <Select
         name="form-field-name"
         value={this.state.value}
-        placeholder = "Select Location..."
+        placeholder = "Select Slot..."
         onChange={this.handleChange}
         options={[
-          { value: 'l1' , label: 'PowerPlay 1 (Near RxDx)' },
-          { value: 'l2' , label: 'PowerPlay 2 (Behind SAP)' },
+          { value: 's0', label: '12:00 PM - 1:00 PM' },
+          { value: 's1', label: '1:00 PM - 2:00 PM' },
+          { value: 's2', label: '2:00 PM - 3:00 PM' },
+        { value:'s3', label: '3:00 PM - 4:00 PM' },
+        { value: 's4', label: '4:00 PM - 5:00 PM' },
+        { value: 's5', label: '5:00 PM - 6:00 PM' },
+        { value: 's6', label: '6:00 PM - 7:00 PM' },
         ]}
       />
     );
   }
 } 
 
-export default Filterbar;
+export default SlotFilter;
